fa8b5a6fb655cd12363a33a6e62f3729f4f55999
Noman Khan <114480394+nomank3797@users.noreply.github.com>
2024-05-15 01:09:09 +0900

    Update run_forecasting.py

--- run_forecasting.py
+++ run_forecasting.py
@@ -3,31 +3,28 @@ import pandas as pd
 import data_processing
 import db_net_model
 
-# load all data
-dataset = pd.read_csv('raw data/household_power_consumption.txt', sep=';', header=0, low_memory=False, infer_datetime_format=True, parse_dates={'datetime':[0,1]}, index_col=['datetime'])
+# Load all data
+# Assuming 'household_power_consumption.txt' is in the 'raw data' directory
+dataset = pd.read_csv('raw data/household_power_consumption.txt', sep=';', header=0, low_memory=False,
+                      infer_datetime_format=True, parse_dates={'datetime': [0, 1]}, index_col=['datetime'])
 
-# choose data frequency
+# Choose data frequency
 frequency = 'D'
 
-# choose a number of time steps
+# Choose a number of time steps for input and output
 n_steps_in, n_steps_out = 2, 1
 
-# epochs
+# Number of epochs for training
 epochs = 300
 
-# define the filename for the CSV file
+# Define the filename for the CSV file to save predictions
 file_name = 'dummy_actual_predicted_values.csv'
 
-# clean the daa
+# Clean the data
 normalized_data = data_processing.clean_data(dataset, frequency)
 
-# convert into input/output
+# Convert data into input/output sequences
 X, y = data_processing.split_sequences(normalized_data, n_steps_in, n_steps_out)
 
-# model training and test
+# Model training and testing
 db_net_model.db_net_model(X, y, n_steps_in, n_steps_out, epochs, file_name)
-
-
-
-
-
